69b9c4ea6e3092566f5ee1d32e4959bc2810dbe0
rvdforst <rolf.v.d.forst@fau.de>
2024-02-23 11:51:31 +0100

    Update Utils.php

--- includes/Utils.php
+++ includes/Utils.php
@@ -4,53 +4,14 @@ namespace RRZE\AccessControl;
 
 defined('ABSPATH') || exit;
 
-/**
- * [Utils description]
- */
 class Utils
 {
-    /**
-     * Log errors by writing to the debug.log file.
-     */
-    public static function debug($input, string $level = 'i')
+    public static function encodeEmail($email)
     {
-        if (!defined('WP_DEBUG') || !WP_DEBUG) {
-            return;
+        $output = '';
+        for ($i = 0; $i < mb_strlen($email); $i++) {
+            $output .= '&#' . ord($email[$i]) . ';';
         }
-        if (in_array(strtolower((string) WP_DEBUG_LOG), ['true', '1'], true)) {
-            $logPath = WP_CONTENT_DIR . '/debug.log';
-        } elseif (is_string(WP_DEBUG_LOG)) {
-            $logPath = WP_DEBUG_LOG;
-        } else {
-            return;
-        }
-        if (is_array($input) || is_object($input)) {
-            $input = print_r($input, true);
-        }
-        switch (strtolower($level)) {
-            case 'e':
-            case 'error':
-                $level = 'Error';
-                break;
-            case 'i':
-            case 'info':
-                $level = 'Info';
-                break;
-            case 'd':
-            case 'debug':
-                $level = 'Debug';
-                break;
-            default:
-                $level = 'Info';
-        }
-        error_log(
-            date("[d-M-Y H:i:s \U\T\C]")
-                . " WP $level: "
-                . basename(__FILE__) . ' '
-                . $input
-                . PHP_EOL,
-            3,
-            $logPath
-        );
+        return $output;
     }
 }
