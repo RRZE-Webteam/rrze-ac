3af7765bda0e54eda08d3e40997ff1b479f45924
rvdforst <rolf.v.d.forst@fau.de>
2024-03-01 17:40:38 +0100

    Update Utils.php

--- includes/Utils.php
+++ includes/Utils.php
@@ -6,12 +6,62 @@ defined('ABSPATH') || exit;
 
 class Utils
 {
+    /**
+     * Check if the plugin is active.
+     * @return boolean
+     */
+    public static function isPluginActive(string $plugin): bool
+    {
+        include_once ABSPATH . 'wp-admin/includes/plugin.php';
+        return is_plugin_active($plugin);
+    }
+
+    /**
+     * Check if the plugin is active for network.
+     * @return boolean
+     */
+    public static function isPluginActiveForNetwork(string $plugin): bool
+    {
+        include_once ABSPATH . 'wp-admin/includes/plugin.php';
+        return is_plugin_active_for_network($plugin);
+    }
+
+    /**
+     * Encode email address
+     * @param string $email
+     * @return string
+     */
     public static function encodeEmail($email)
     {
         $output = '';
         for ($i = 0; $i < mb_strlen($email); $i++) {
             $output .= '&#' . ord($email[$i]) . ';';
         }
+        return $output;
+    }
+
+    /**
+     * Encrypt and decrypt string
+     * @param string $string
+     * @param string $action
+     * @return string
+     */
+    public static function crypt($string, $action = 'encrypt')
+    {
+        $secretKey = AUTH_KEY;
+        $secretSalt = AUTH_SALT;
+
+        $output = false;
+        $encryptMethod = 'AES-256-CBC';
+        $key = hash('sha256', $secretKey);
+        $salt = substr(hash('sha256', $secretSalt), 0, 16);
+
+        if ($action == 'encrypt') {
+            $output = base64_encode(openssl_encrypt($string, $encryptMethod, $key, 0, $salt));
+        } else if ($action == 'decrypt') {
+            $output = openssl_decrypt(base64_decode($string), $encryptMethod, $key, 0, $salt);
+        }
+
         return $output;
     }
 }
