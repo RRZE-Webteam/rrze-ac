c2e488c051d055206d4b68859acc33e63f1ef217
Rolf <rolf.v.d.forst@fau.de>
2025-01-28 10:16:54 +0100

    Update rrze-ac.php

--- rrze-ac.php
+++ rrze-ac.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name:        RRZE Access Control
 Plugin URI:         https://gitlab.rrze.fau.de/rrze-webteam/rrze-ac
-Version:            3.1.2
+Version:            3.1.3
 Description:        Allows protection of files/documents through user and network related functions.
 Author:             RRZE Webteam
 Author URI:         https://blogs.fau.de/webworking/
